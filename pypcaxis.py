0313eceac203e538e1abcfc5e6e3ca93853b1375
Pedro-Juan Ferrer Matoses <pedro.ferrer@omniumie.com>
2013-05-08 11:59:09 +0200

    Changed the first split condition in order to accept also descriptions that include ";" in the text.

--- pypcaxis.py
+++ pypcaxis.py
@@ -59,7 +59,7 @@ def parse(path):
 
 def read_data(path):
     return [t.strip() for t in
-            open(path).read().decode('ISO-8859-1').split(';')]
+            open(path).read().decode('ISO-8859-1').split(';\n')]
 
 
 def create_dimension(title, values):
